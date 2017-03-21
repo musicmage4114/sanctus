c56dc1229876ecf5a31ca0ae104a76bf95e19d65
Zachary Lemmon <musicmage4114@aim.com>
2017-03-20 23:48:26 -0400

    Set up basic home page for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'general_pages#home'
+
+  get '/help', to: 'general_pages#help'
+  get '/about', to: 'general_pages#about'
+  get '/contact', to: 'general_pages#contact'
 end
