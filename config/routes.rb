3ad7889c6601eedd46d0ab74c362de9675192fe2
Zachary Lemmon <musicmage4114@aim.com>
2017-04-25 21:48:37 -0400

    Add static page comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'general_pages#home'
 
+  # Named routes to static pages.
   get '/help', to: 'general_pages#help'
   get '/about', to: 'general_pages#about'
   get '/contact', to: 'general_pages#contact'
+  
 end
