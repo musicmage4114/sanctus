00708fd6da34141a35644cc38b996b9e7b7b4eb7
Zachary Lemmon <musicmage4114@aim.com>
2017-04-25 22:45:06 -0400

    Add tentative Sessions MVC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  # Finds a user by user ID.
+  def get_user
+    @user = User.find_by(id: :user_id)
+  end
+  
+  private
+  
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        redirect_to login_url
+      end
+    end
+  
 end
